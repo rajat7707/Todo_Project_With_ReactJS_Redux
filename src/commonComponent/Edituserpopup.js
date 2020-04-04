eeaec0a3a2c2cdd5dd26df953f612d5519508727
rajat7707 <42389833+rajat7707@users.noreply.github.com>
2020-04-04 13:16:05 +0530

    Update Edituserpopup.js

--- src/commonComponent/Edituserpopup.js
+++ src/commonComponent/Edituserpopup.js
@@ -7,15 +7,29 @@ import { connect } from 'react-redux';
 class Edituserpopup extends Component{
 
 	state = {
-		addType : this.props.section
+		addType : this.props.section,
+		heading : "",
 	}
+
+	UNSAFE_componentWillMount(){
+		if(this.props.section === "ADD_TODO_POP" && this.props.singleTodoData.time !== "" && this.props.singleTodoData !== undefined){
+			this.setState({heading : "Edit Todo"}) ;
+		}else if(this.props.section === "ADD_TODO_POP"){
+			this.setState({heading : "Add Todo"}) ;
+		}else if(this.props.section === "ADD_USER_POP" && this.props.singleUserData.email !== "" && this.props.singleUserData !== undefined){
+			this.setState({heading : "Edit User"}) ;
+		}else if(this.props.section === "ADD_USER_POP"){
+			this.setState({heading : "Add User"}) ;
+		}	
+	}
+	
 	
 	render(){
 		return (
 	
 		    <Modal show={this.props.show} onHide={this.props.handleClose}>
 		        <Modal.Header closeButton>
-		          <Modal.Title>Modal heading</Modal.Title>
+		          <Modal.Title>{this.state.heading}</Modal.Title>
 		        </Modal.Header>
 
 		        <Modal.Body>
@@ -41,7 +55,8 @@ const  mapStateToProps = (state) => {
     	showUserSection : state.showUserSection,
     	showTodosSection : state.showTodosSection,
     	show : state.show,
-    	singleUserData : state.singleUserData
+    	singleUserData : state.singleUserData,
+    	singleTodoData : state.singleTodoData
     }
 }
 
