31bede8618f81c426f29f8413f3a682e44954c4c
rajat7707 <42389833+rajat7707@users.noreply.github.com>
2020-04-06 23:14:45 +0530

    Update Userlist.js

--- src/userComponent/Userlist.js
+++ src/userComponent/Userlist.js
@@ -3,16 +3,26 @@ import DatatablePage from '../commonComponent/info_list';
 import Edituserpopup from '../commonComponent/Edituserpopup';
 import Flashmessage from '../commonComponent/flashmessage';
 import { NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
-function Userlist(props) {
+export default function Userlist() {
 
-  props.usersData.map((value, index) => {
+  const selector = useSelector( (state) => {
+      return {
+          flashMsg        : state.flashMsg,
+          errorType       : state.errorType,
+          usersData       : state.usersData,
+          userPopup       : state.userPopup, 
+      }
+  });
+  const dispatchAction = useDispatch();
+
+  selector.usersData.map((value, index) => {
       value.action = 
             <div className = "">
-                <NavLink to = "#" onClick={(e) => props.handleEdit(e, value, value.id)} className="textInline" title="Edit" >Edit</NavLink>
+                <NavLink to = "#" onClick={((e) => dispatchAction({type : "EDIT", singleUserData: value, id : value.id}))} className="textInline" title="Edit" >Edit</NavLink>
                  <b style = {{marginLeft : "20px"}}>|</b>
-                <NavLink to = "#" onClick={(e) => props.handleDelete(e, value.id)} className="textInline" title="Delete" style = {{marginLeft : "20px"}} >Delete</NavLink>
+                <NavLink to = "#" onClick={((e) => dispatchAction({type : "DELETE", id : value.id, deleteType : "USER"}))} className="textInline" title="Delete" style = {{marginLeft : "20px"}} >Delete</NavLink>
             </div>
 
       return value;
@@ -20,29 +30,10 @@ function Userlist(props) {
 
   return (
     <div>
-      <button type="button" className="btn btn-primary" onClick = {(e) => props.showPopUp(e, "USER_POP")}>Create User</button><br /><br />
-      { props.flashMsg !== "" && props.flashMsg !== undefined && <Flashmessage flashMsg={props.flashMsg} type = {props.errorType} />}
-      <DatatablePage type = "userInfo" data = {props.usersData} />
-      { props.userPopup && <Edituserpopup section = "ADD_USER_POP" />}
+      <button type="button" className="btn btn-primary" onClick = {((e) => dispatchAction({type : "SHOW_POPUP", popType: "USER_POP"}))}>Create User</button><br /><br />
+      { selector.flashMsg !== "" && selector.flashMsg !== undefined && <Flashmessage flashMsg={selector.flashMsg} type = {selector.errorType} />}
+      <DatatablePage type = "userInfo" data = {selector.usersData} />
+      { selector.userPopup && <Edituserpopup section = "ADD_USER_POP" />}
     </div>
   );
 }
-
-const  mapStateToProps = (state) => {
-    return {
-      flashMsg        : state.flashMsg,
-      errorType       : state.errorType,
-      usersData       : state.usersData,
-      userPopup       : state.userPopup,
-    }
-}
-
-const  mapDispatchToProps = (dispatch) => {
-    return {
-      handleEdit    : (e, value, id) => dispatch({type : "EDIT", singleUserData: value, id : id}),
-      handleDelete  : (e, id) => dispatch({type : "DELETE", id: id, deleteType : "USER"}),
-      showPopUp     : (e, popType) => dispatch({type : "SHOW_POPUP", popType:popType}),
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Userlist);
